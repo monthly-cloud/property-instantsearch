52f540fefe510dcb8a222d2d04bfe0f52ce36628
sl0wik <slowik.michael@gmail.com>
2017-11-18 12:10:13 +0100

    instantsearch and maps added to js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,13 +9,23 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import AlgoliaComponents from 'vue-instantsearch';
+Vue.use(AlgoliaComponents);
+
+import * as VueGoogleMaps from 'vue2-google-maps';
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: 'AIzaSyBvWE_sIwKbWkiuJQOf8gSk9qzpO96fhfY',
+    }
+});
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('search-result', require('./components/SearchResult.vue'));
 
 const app = new Vue({
     el: '#app'
