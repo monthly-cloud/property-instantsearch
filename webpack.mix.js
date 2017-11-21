f8a60c56c9a0f7bf08e6382c26e402034d407b50
wlecki <ridwojtas@gmail.com>
2017-11-21 13:36:44 +0100

    fixing bug - not working mix.version() for hot mode

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,9 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .version();
+    .sass('resources/assets/sass/app.scss', 'public/css');
+
+// mix.version is breaking HOT mode
+if (mix.inProduction()) {
+    mix.version();
+}
