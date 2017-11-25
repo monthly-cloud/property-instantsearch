77974a02df6b08e21287eb5e58bf744a3108c9be
sl0wik <slowik.michael@gmail.com>
2017-11-25 15:59:18 +0100

    results seeder and transformer

--- app/Models/Result.php
+++ app/Models/Result.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Result extends Model
 {
-    //
+    use Searchable;
+
+    public function toSearchableArray()
+    {
+        $record = $this->toArray();
+
+        unset($record['created_at'], $record['updated_at']);
+        $record['available_from'] = Carbon::today()->timestamp;
+        $record['_geoloc'] = [
+            'lat' => (float) $record['lat'],
+            'lng' => (float) $record['lng'],
+        ];
+
+        return $record;
+    }
 }
