c7f7aeb6f1b2d33ac578b269b0f6a2e8b6caad85
wlecki <ridwojtas@gmail.com>
2018-04-14 17:12:21 +0200

    adding row with all filters for algolia search

--- resources/assets/js/components/FiltersRow.vue
+++ resources/assets/js/components/FiltersRow.vue
@@ -1,29 +1,97 @@
 <template>
     <div class="filters-container">
         <button
-        class="btn btn-default" disabled>
-            Date
+        :class="filtersExpanded && activeFilter === 'available_from' ? 'btn btn-default active' : 'btn btn-default'"
+        @click="openDateFilter()">
+            Available from
         </button>
-        <button
-        :class="filtersExpanded ? 'btn btn-default active' : 'btn btn-default'"
-        @click="openRoomFilter()">
-            Room Filter
-        </button>
-
         <div
-        v-if="filtersExpanded"
+        v-show="filtersExpanded && activeFilter === 'available_from'"
+        class="filters-container__expanded-filter filters-container__expanded-filter--date"
+        :style="'left: ' + horizontalAdjustment + 'px'">
+            <ais-date-picker
+            name="available_from"/>
+        </div>
+
+        <button
+        :class="filtersExpanded && activeFilter === 'roomType' ? 'btn btn-default active' : 'btn btn-default'"
+        @click="openRoomFilter()">
+            Room type
+        </button>
+        <div
+        v-show="filtersExpanded && activeFilter === 'roomType'"
         class="filters-container__expanded-filter"
         :style="'left: ' + horizontalAdjustment + 'px'">
             <ais-refinement-list
-            v-if="activeFilter === 'roomType'"
             attribute-name="room_type">
             </ais-refinement-list>
+        </div>
+
+        <button
+        :class="filtersExpanded && activeFilter === 'occupancy_limit' ? 'btn btn-default active' : 'btn btn-default'"
+        @click="openOccupancyFilter()">
+            Guests
+        </button>
+        <div
+        v-show="filtersExpanded && activeFilter === 'occupancy_limit'"
+        class="filters-container__expanded-filter"
+        :style="'left: ' + horizontalAdjustment + 'px'">
+            <ais-numeric-menu
+            name="occupancy_limit">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+            </ais-numeric-menu>
+        </div>
+
+        <button
+        :class="filtersExpanded && activeFilter === 'bedrooms' ? 'btn btn-default active' : 'btn btn-default'"
+        @click="openBedroomsFilter()">
+            Bedrooms
+        </button>
+        <div
+        v-show="filtersExpanded && activeFilter === 'bedrooms'"
+        class="filters-container__expanded-filter"
+        :style="'left: ' + horizontalAdjustment + 'px'">
+            <ais-numeric-menu
+            name="bedrooms">
+                <option
+                label="Studio (0)">
+                    0
+                </option>
+                <option
+                label="1 bedroom">
+                    1
+                </option>
+                <option
+                label="2 bedrooms">
+                    2
+                </option>
+                <option
+                label="3 bedrooms">
+                    3
+                </option>
+            </ais-numeric-menu>
+        </div>
+
+        <button
+        :class="filtersExpanded && activeFilter === 'price' ? 'btn btn-default active' : 'btn btn-default'"
+        @click="openPriceRangeFilter()">
+            Price range
+        </button>
+        <div
+        v-show="filtersExpanded && activeFilter === 'price'"
+        class="filters-container__expanded-filter"
+        :style="'left: ' + horizontalAdjustment + 'px'">
+            <ais-price-range attribute-name="price" currency="USD"></ais-price-range>
         </div>
 
         <div
         v-if="filtersExpanded"
         class="expanded-filter__overlay"
-        @click="cleanFilters()"></div>
+        @click="hideFilters()"></div>
     </div>
 </template>
 
@@ -37,14 +105,34 @@
             }
         },
         methods: {
-            cleanFilters() {
+            hideFilters() {
                 this.filtersExpanded = false;
                 this.activeFilter = undefined;
             },
+            openDateFilter() {
+                this.filtersExpanded ? this.filtersExpanded = false : this.filtersExpanded = true;
+                this.horizontalAdjustment = 0;
+                this.activeFilter = 'available_from';
+            },
             openRoomFilter() {
                 this.filtersExpanded ? this.filtersExpanded = false : this.filtersExpanded = true;
-                this.horizontalAdjustment = 65;
+                this.horizontalAdjustment = 123;
                 this.activeFilter = 'roomType';
+            },
+            openOccupancyFilter() {
+                this.filtersExpanded ? this.filtersExpanded = false : this.filtersExpanded = true;
+                this.horizontalAdjustment = 228;
+                this.activeFilter = 'occupancy_limit';
+            },
+            openBedroomsFilter() {
+                this.filtersExpanded ? this.filtersExpanded = false : this.filtersExpanded = true;
+                this.horizontalAdjustment = 309;
+                this.activeFilter = 'bedrooms';
+            },
+            openPriceRangeFilter() {
+                this.filtersExpanded ? this.filtersExpanded = false : this.filtersExpanded = true;
+                this.horizontalAdjustment = 407;
+                this.activeFilter = 'price';
             }
         }
     }
@@ -74,7 +162,6 @@
         left: 0;
         z-index: 10;
         width: 200px;
-        height: 200px;
         border: 1px solid rgba(0, 0, 0, 0.2);
         border-radius: 4px;
         box-shadow: 0 14px 36px 2px rgba(0, 0, 0, 0.15);
@@ -82,6 +169,11 @@
         overflow-y: auto;
         white-space: normal;
         background-color: #fff;
+    }
+
+    .filters-container__expanded-filter--date {
+        min-height: 363px;
+        min-width: 353px;
     }
 
     .expanded-filter__overlay {
